1102c66c4096e1d18778b6df7883740678cebed8
wangtianlun <810394177@qq.com>
2021-08-01 14:19:24 +0800

    feat: 添加路由配置

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './routes/routes.dart';
 import 'pages/home/index.dart';
 
 void main() {
@@ -14,7 +15,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomeIndexPage(),
+      initialRoute: '/',
+      onGenerateRoute: onGenerateRoute,
     );
   }
 }
